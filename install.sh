aab38690add603619f045017138b3c87be57e9ed
Khaidar Bikmaev <khaidar@bikmaev.com>
2023-09-13 21:02:49 +0400

    all my homies hate git

--- install.sh
+++ install.sh
@@ -31,7 +31,12 @@ rm -rf ~/.bash_profile
 rm -rf ~/.tmux.conf
 rm -rf ~/.vimrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
+if [[ "$platform" == "Linux" ]]; then
+    ln -s ~/dotfiles/.gitconfig.linux ~/.gitconfig
+elif [[ "$platform" == "Darwin" ]]; then
+    ln -s ~/dotfiles/.gitconfig.macos ~/.gitconfig
+fi
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.vimrc ~/.vimrc
