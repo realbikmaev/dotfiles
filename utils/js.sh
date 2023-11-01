23539e10676abf30c952ffeee36158eb85ac7d2d
Khaidar Bikmaev <khaidar@bikmaev.com>
2023-11-01 18:34:18 +0400

    wtf

--- utils/js.sh
+++ utils/js.sh
@@ -2,11 +2,10 @@ export NVM_DIR="$HOME/.nvm"
 if [[ "$platform" == 'Linux' ]]; then
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
 elif [[ "$platform" == 'Darwin' ]]; then
-
     [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
     [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+    nvm use --lts >/dev/null
 fi
 
 alias sr="npm run dev"
